6363917f590a8946e9e5a990d956c73540773225
Jake Bowers <jake@jakebowers.org>
2022-02-01 09:14:45 -0600

    Create file that grabs datasets used in the final analysis and saves them as csv

---
+++ src/R/extract_post_matching_data.R
@@ -0,0 +1,17 @@
+## Extract datasets used in final analyses as csv files for use in other
+## programs
+
+library(tidyverse)
+library(here)
+source(here("src", "R", "000_constants_and_utils.R"))
+source(here("src/R", "rmd_setup.R"))
+
+## Study 1 final dataset is called dat3
+load(file = here(MATCHES_DIR, "outcome_analysis_study1.rda"),verbose=TRUE)
+write_csv(dat3,file=here(MATCHES_DIR, "study1_dat_plus_matches_dat3.csv"))
+
+## Study 2 final dataset is called dat5
+load(file = here(MATCHES_DIR, "outcome_analysis_study2.rda"),verbose=TRUE)
+write_csv(dat5,file=here(MATCHES_DIR, "study1_dat_plus_matches_dat5.csv"))
+
+
