b1b9a34a3de270ff0e7f5bf9fbf77334d4a5dbfb
Jake Bowers <jake@jakebowers.org>
2022-02-01 09:17:12 -0600

    Change save path

--- src/R/extract_post_matching_data.R
+++ src/R/extract_post_matching_data.R
@@ -8,10 +8,10 @@ source(here("src/R", "rmd_setup.R"))
 
 ## Study 1 final dataset is called dat3
 load(file = here(MATCHES_DIR, "outcome_analysis_study1.rda"),verbose=TRUE)
-write_csv(dat3,file=here(MATCHES_DIR, "study1_dat_plus_matches_dat3.csv"))
+write_csv(dat3,file=here(DATA_DIR, "study1_dat_plus_matches_dat3.csv"))
 
 ## Study 2 final dataset is called dat5
 load(file = here(MATCHES_DIR, "outcome_analysis_study2.rda"),verbose=TRUE)
-write_csv(dat5,file=here(MATCHES_DIR, "study1_dat_plus_matches_dat5.csv"))
+write_csv(dat5,file=here(DATA_DIR, "study1_dat_plus_matches_dat5.csv"))
 
 
